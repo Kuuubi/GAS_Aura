7156b7ded3c04855860df03caf4af79c3c7b9173
Kuuubi <1145356956@qq.com>
2024-11-24 04:38:20 +0800

    执行计算，格挡几率，护甲和护甲穿透

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,3 +3,105 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
+#include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+//捕获定义结构体
+struct AuraDamageStatics
+{
+	//捕获宏声明指定捕获属性
+	//护甲
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	//护甲穿透
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
+	//格挡几率
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+	AuraDamageStatics()
+	{
+		//捕获定义参数宏
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, ArmorPenetration, Source, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false)
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	//添加到属性捕获数组
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	//获取ASC
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	//获取Avatar
+	//如果ASC有效就获取Avatar并设置为空指针
+	AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	//获取Spec信息
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	//获取标签
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	
+	//数值计算聚合器
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	
+	//从 由调用者设置 获取Damage标签的值
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+
+	//计算捕获属性的数值
+	/* 格挡几率 */
+	//目标格挡几率
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
+	//夹紧属性值
+	TargetBlockChance = FMath::Max<float>( 0.0f, TargetBlockChance);
+	//判断攻击是否被成功阻挡，如果阻挡成功，伤害将减半
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage;
+
+	/* 护甲和护甲穿透 */
+	//目标护甲
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	//夹紧属性值
+	TargetArmor = FMath::Max<float>(0.f, TargetArmor);
+
+	//来源护甲穿透
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluateParameters, SourceArmorPenetration);
+	//夹紧属性值
+	SourceArmorPenetration = FMath::Max<float>(0.f, SourceArmorPenetration);
+
+	//护甲穿透会忽略目标护甲百分比
+	//有效护甲值
+	const float EffectiveArmor = TargetArmor *= ( 100 - SourceArmorPenetration * 0.25f) / 100.f;
+	//最终伤害
+	Damage *= ( 100 - EffectiveArmor * 0.333f ) / 100.f;
+	
+	//计算方式
+	//const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	//输出计算结果
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+
+}
